d2094b84671589dc121ef6341a2064744533ca5c
JB Lee <103769156+jeongbeom98@users.noreply.github.com>
2022-09-14 11:27:27 +0900

    Update boxer data crawling.py

--- boxer data crawling.py
+++ boxer data crawling.py
@@ -1,5 +1,6 @@
 # 필요한 모듈을 임포트 합니다
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 #현재 설치되어있는 Chrome 버전과 동일한 Chromedriver을 다운받습니다
@@ -15,13 +16,26 @@ time.sleep(4)
 
 #월드 랭킹으로 이동합니다
 driver.get("https://boxrec.com/en/ratings?sex=M")
-time.sleep(3)
+time.sleep(4)
 
-#현 페이지의 가장 상단에 위치한 선수부터 최하단의 선수의 페이지까지 이동합니다.
-#한 페이지에 50명의 선수가 있습니다.
-#페이지는 1페이지부터 458 페이지까지로 제한합니다.
-for count in range(50):
-    boxerN = int(count)
-    boxerRanking = ('//*[@id="se' + str(boxerN) + '"]/td[2]/a')
-    driver.find_element_by_xpath(boxerRanking).click()
-    
+#세계 랭킹 페이지를 이동합니다.
+#페이지는 1페이지부터 200 페이지까지로 제한합니다.
+for page in range(2,4):
+
+    # 현 페이지의 가장 상단에 위치한 선수부터 최하단의 선수의 페이지까지 이동합니다.
+    # 한 페이지에 50명의 선수가 있습니다.
+    for count in range(2):
+        boxerN = int(count)
+        boxerRanking = ('//*[@id="se' + str(boxerN) + '"]/td[2]/a')
+        driver.find_element_by_xpath(boxerRanking).click()
+
+        #추가 필요: 현 페이지 복서의 데이터를 csv 형태로 저장합니다.
+        #저장할 데이터:
+
+        #이전 페이지로 이동합니다.
+        time.sleep(1)
+        driver.back()
+
+    #다음 페이지로 이동합니다.
+    driver.find_element_by_xpath('// *[ @ id = "pageOuter"] / div / div[9] / div[1] / input').send_keys(page)
+    driver.find_element_by_xpath('// *[ @ id = "pageOuter"] / div / div[9] / div[1] / input').send_keys(Keys.ENTER)
